b4e243edaba2c85e76c1af0a425e7555e2d19ec9
Wanderson Chaves <wandersonscpibr@gmail.com>
2022-05-08 17:21:23 -0300

    sent email

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,40 @@
 import express from "express";
+import { prisma } from "./prisma";
+import nodemailer from "nodemailer";
 
 const app = express();
 
-// GET, POST, PUT, PATCH, DELETE
+app.use(express.json());
 
-app.post("/feedbacks", (req, res) => {
-  return res.send("Hello World");
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "060218042f2073",
+    pass: "2a7b116256cd07",
+  },
+});
+
+app.post("/feedbacks", async (req, res) => {
+  const { type, comment, screenshot } = req.body;
+
+  const feedback = await prisma.feedback.create({
+    data: { type, comment, screenshot },
+  });
+
+  transport.sendMail({
+    from: "Equipe Chav3x <oi@feed.com>",
+    to: "Wanderson Chaves <wanderson@teste.com>",
+    subject: "Test Feedback",
+    html: [
+      `<div style='font-family: sans-serif; font-size: 16px; color: #111;'>`,
+      `<p>Tipo do feedback: ${type}</p>`,
+      `<p>Comentario: ${comment}</p>`,
+      `</div>`,
+    ].join("\n"),
+  });
+
+  return res.status(201).json({ data: feedback });
 });
 
 app.listen(3333, () => {
