4ff3e369d81e935c664caa214d658f120f7fe8e6
Wanderson Chaves <wandersonscpibr@gmail.com>
2022-05-08 23:05:11 -0300

    testing snippets

---
+++ mobile/App.tsx
@@ -0,0 +1,13 @@
+import { StatusBar } from "expo-status-bar";
+import { View } from "react-native";
+import { Widget } from "./src/components/Widget";
+import { theme } from "./src/theme";
+
+export default function App() {
+  return (
+    <View style={{ flex: 1, backgroundColor: theme.colors.background }}>
+      <Widget />
+      <StatusBar style="light" backgroundColor="transparent" translucent />
+    </View>
+  );
+}
